52cdd9131d2193c994ef89b11902d8659a6be7a1
Armin Ronacher <armin.ronacher@active-4.com>
2025-06-22 20:36:28 +0200

    vibetunnel -> VibeTunnel

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
             className="w-20 h-20 md:w-25 md:h-25"
           />
           <h1 className="text-5xl md:text-6xl font-bold text-white">
-            vibetunnel
+            VibeTunnel
           </h1>
         </header>
 
@@ -73,7 +73,7 @@ export default function Home() {
                 <div className="w-3 h-3 rounded-full bg-green-500"></div>
               </div>
               <div className="flex-grow text-center text-sm text-gray-400">
-                vibetunnel ~ zsh
+                VibeTunnel ~ zsh
               </div>
             </div>
             <div className="w-full h-[400px] md:h-[500px] bg-black">
